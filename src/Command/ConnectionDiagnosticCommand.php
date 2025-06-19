f6595707e3003c3632364b0604ad6aaa5961e2af
Jérôme Tamarelle <jerome@tamarelle.net>
2025-06-19 21:38:21 +0200

    Remove more code from the command

--- src/Command/ConnectionDiagnosticCommand.php
+++ src/Command/ConnectionDiagnosticCommand.php
@@ -4,20 +4,10 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MongoDBBundle\Command;
 
-use Doctrine\Bundle\MongoDBBundle\DataCollector\ConnectionDiagnostic;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Contracts\Service\ServiceProviderInterface;
-use Throwable;
-
-use function array_diff;
-use function array_keys;
-use function implode;
-use function sprintf;
 
 /** @internal */
 #[AsCommand(
@@ -26,26 +16,8 @@ use function sprintf;
 )]
 final class ConnectionDiagnosticCommand extends Command
 {
-    /** @param ServiceProviderInterface<ConnectionDiagnostic> $diagnostics */
-    public function __construct(private readonly ServiceProviderInterface $diagnostics)
-    {
-        parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this->addOption('connection', 'c', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'The name of the connection to diagnose. If not specified, all connections will be diagnosed.', [], $this->getConnectionNames(...));
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
         return Command::SUCCESS;
-    }
-
-    /** @return list<string> */
-    private function getConnectionNames(): array
-    {
-        return array_keys($this->diagnostics->getProvidedServices());
     }
 }
